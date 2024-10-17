d50fc58155cc041b38df3e6bee48578c0a2bbc25
albinsanthosh <albin.santhosh98@gmail.com>
2024-10-17 19:36:27 +0530

    SimpleDemo - application with classes

---
+++ SimpleDemo/src/main/java/com/camunda/demo/SimpleDemo/CheckWeatherDelegate.java
@@ -0,0 +1,19 @@
+package com.camunda.demo.SimpleDemo;
+
+import java.util.Random;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CheckWeatherDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		Random rando=new Random();
+		
+		execution.setVariable("name", "Naill");
+		execution.setVariable("weatherOk", rando.nextBoolean());
+	}
+
+}
